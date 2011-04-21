115e5e9b61066b1e41bdd5e29ac05da58696e033
Matthias Hryniszak <padcom@gmail.com>
2011-04-21 16:09:43 +0200

    Updated plugin version to 1.0.5

--- JsonRestApiGrailsPlugin.groovy
+++ JsonRestApiGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import org.grails.plugins.rest.JSONApiRegistry
 
 class JsonRestApiGrailsPlugin {
     // the plugin version
-    def version = "1.0.4"
+    def version = "1.0.5"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.0 > *"
     // the other plugins this plugin depends on
